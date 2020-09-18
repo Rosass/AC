cbc9c537cc635ff0b7fc4606d9281ed6080936b9
Alvaro <sanchezrmz410@gmail.com>
2020-09-18 00:50:39 -0500

    hola mundo

--- resources/views/jefe/index.blade.php
+++ resources/views/jefe/index.blade.php
@@ -58,7 +58,7 @@
             <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
           </svg> Añadir</a>
         </div>
-
+<h1>hola mundo</h1>
     <div class="card-body">
         <form>
             <div class="row justify-content-md-between">
